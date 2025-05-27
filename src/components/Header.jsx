5dded59e1561d31c814d8539551c196af56f0fe4
Leandro Mayron <leandromayron9999@gmail.com>
2025-05-27 13:05:41 -0300

    adicionado as bibliotecas de estilização

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logoHedeader from '../assets/logo-header.svg';
+import 'primeicons/primeicons.css'
 
 const Header = () => {
     return (
         <header>
             <img src={logoHedeader} alt="Digital Store" />
             <form>
-                <input type="text" id="pesquisa" name="pesquisa" placeholder='pesquisar produtos'/>
-                <img src="../assets/Search.png" alt="seach" />
+                <input type="text" id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' className='pi pi-search'/>
             </form>
         </header>
     )
