10e67cab2ec5aa87f1117b010467efdd0832785d
Leandro Mayron <leandromayron9999@gmail.com>
2025-05-29 06:48:56 -0300

    installado tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import logoHedeader from '../assets/logo-header.svg';
 import 'primeicons/primeicons.css'
 
+
 const Header = () => {
     return (
-        <header>
+        <header className='flex justify-around items-center'>
             <img src={logoHedeader} alt="Digital Store" />
             <form>
-                <input type="text" id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' className='pi pi-search'/>
+                <input type="text" id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' className=' pi pi-search p-1 flex align-center border-1 rounded'/>
+                
             </form>
         </header>
     )
