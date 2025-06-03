a493b6f18cc20ecb13fa9cb84e099714f1d15a6e
Leandro Mayron <leandromayron9999@gmail.com>
2025-06-03 18:01:11 -0300

    criando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,34 @@ import 'primeicons/primeicons.css'
 import {IconField} from 'primereact/iconfield';
 import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
+import {Menubar} from 'primereact/menubar';
 
 
 
 
 
 const Header = () => {
-
+    const items = [
+        {
+            label: 'Home',
+        },
+        {
+            label: 'Produtos',
+        },
+        {
+            label: 'Categorias'
+        },
+        {
+            label: 'Meus Pedidos'
+        }
+    ];
 
     return (
-        <>
-            <header className='m-10 flex justify-evenly items-center '>
+        <header className='w-full bg-light-gray'>
+            <div className='w-7xl h-20 m-auto flex justify-evenly items-center bg-sucess'>
                 <img src={logoHedeader} alt="Digital Store" />
                     <IconField>
-                        <InputText className='w-100 h-12 bg-light-gray-3 rounded-l-md p-2' id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' />
+                        <InputText className='w-xs h-12 bg-light-gray-3 rounded-l-md p-2' id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' />
                         <button className='w-12 h-12 mt-2 mb-2 bg-light-gray-3  rounded-r-md'>
                             <i className='pi pi-search text-dark-gray-2 '></i>
                         </button>
@@ -24,9 +38,11 @@ const Header = () => {
                 <a className='underline text-dark-gray-2' href="">Cadastre-se</a>
                 <Button className='w-30 h-10 bg-primary rounded text-light-gray-3 font-bold' label='Entrar'/>
                 <i className='pi pi-shopping-cart text-xl text-primary cursor-pointer size' label='Carrinho'></i>
-            </header>
-            
-        </>
+            </div>
+            <div className='w-7xl h-20 m-auto flex bg-success card'>
+                <Menubar className='bg-secondary w-lg' model={items}/>
+            </div>
+        </header>
     )
 }
 
