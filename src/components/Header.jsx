9b3ad615db35966f9a47dc556cb1a197d2242bb9
Leandro Mayron <leandromayron9999@gmail.com>
2025-06-03 14:30:38 -0300

    estilizando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,45 +2,25 @@ import logoHedeader from '../assets/logo-header.svg';
 import 'primeicons/primeicons.css'
 import {IconField} from 'primereact/iconfield';
 import {InputText} from 'primereact/inputtext';
-import {Menubar} from 'primereact/menubar';
+import {Button} from 'primereact/button';
+
+
 
 
 
 const Header = () => {
-    const items = [
-        {
-            label: 'Home',
-            icon: 'pi pi-fw pi-home',
-            command: () => { window.location.href = '/' }
-        },
-        {
-            label: 'Produtos',
-            icon: 'pi pi-fw pi-box',
-            command: () => { window.location.href = '/produtos' }
-        },
-        {
-            label: 'Sobre',
-            icon: 'pi pi-fw pi-info-circle',
-            command: () => { window.location.href = '/sobre' }
-        },
-        {
-            label: 'Contato',
-            icon: 'pi pi-fw pi-envelope',
-            command: () => { window.location.href = '/contato' }
-        }
-    ];
+
 
     return (
         <header className='flex justify-around items-center'>
             <img src={logoHedeader} alt="Digital Store" />
 
 
-                <IconField className='pi pi-search  text-gray-500' id="icon" name="icon">
-                    <InputText className='border-2 border-gray-300 rounded-md p-2' id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' />
+                <IconField className='pi pi-search' id="icon" name="icon">
+                    <InputText className='w-100 bg-light-gray-3 rounded-md p-2' id="pesquisa" name="pesquisa" placeholder='pesquisar produtos' />
                 </IconField>
-                
-                <Menubar model={items}/>
-            
+             <a className='underline text-dark-gray-2' href="">Cadastre-se</a>
+             <Button className='w-30 h-8 bg-primary rounded text-light-gray-3 font-bold' label='Entrar'/>
         </header>
     )
 }
